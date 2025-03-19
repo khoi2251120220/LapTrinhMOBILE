898045e4c057bea03be9d4d4c21bda82eec5aed0
VTruong27012004 <2251120257@ut.edu.vn>
2025-03-20 00:29:53 +0700

    fix loi gradle

--- RestaurantManage/app/build.gradle.kts
+++ RestaurantManage/app/build.gradle.kts
@@ -50,11 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation ("androidx.navigation:navigation-compose:2.7.7")
-<<<<<<< Updated upstream
     implementation("io.coil-kt:coil-compose:2.2.2")
-=======
-    implementation("io.coil-kt:coil-compose:2.0.0")
->>>>>>> Stashed changes
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
