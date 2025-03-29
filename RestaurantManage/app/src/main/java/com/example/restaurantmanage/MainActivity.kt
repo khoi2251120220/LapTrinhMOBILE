47267ef7c5e5aa325031519b99f79e09a9934cff
BìnhTran <tbsk161104@gmail.com>
2025-03-29 16:49:37 +0700

    update MainScreenUser.kt

--- RestaurantManage/app/src/main/java/com/example/restaurantmanage/MainActivity.kt
+++ RestaurantManage/app/src/main/java/com/example/restaurantmanage/MainActivity.kt
@@ -1,19 +1,32 @@
-package com.example.restaurantmanage
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.restaurantmanage.ui.theme.screens.admin.MainScreenAdmin
-import com.example.restaurantmanage.ui.theme.screens.assignment.AssignmentMain
-import com.example.restaurantmanage.ui.theme.screens.user.MainScreenUser
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           // AssignmentMain()
-           // MainScreenUser()
-           // MainScreenAdmin()
+            MaterialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    MainScreenUser(navController = navController)
+                }
+            }
         }
     }
+}
+
+private fun MainActivity.MainScreenUser(navController: NavHostController) {
+    TODO("Not yet implemented")
 }
